bfa1f1ab310de1c499004c6f6235083087a3486c
suragys <suragysms@gmail.com>
2017-01-16 12:36:10 -0800

    Initial commit

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+/**
+ * Created by surag on 1/16/17.
+ */
+public class LinkedList {
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    Node head;
+
+}
